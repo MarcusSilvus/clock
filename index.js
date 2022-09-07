ed1c05833106d20d2469997255912dece85cb4e8
Mark Silva <mark.allen.silva@gmail.com>
2022-09-06 20:52:26 -0700

    enacted new method to get current time

---
+++ index.js
@@ -0,0 +1,23 @@
+const time = document.getElementById("time");
+const date = document.getElementById("date");
+
+const dateFormat = {weekday: 'long', month: 'short', day: 'numeric'};
+const today = new Date();
+
+
+const hours = convertHours(today.getHours());
+const minutes = today.getMinutes();
+const seconds = addZero(today.getSeconds());
+
+function convertHours(num) {
+  return num > 12 ? num - 12 : num;
+}
+
+function addZero(num) {
+  return num < 10 ? `0${num}`: num;
+}
+
+const currentTime = `${hours}:${minutes}:${seconds}`;
+
+time.innerHTML = currentTime;
+date.innerHTML = today.toLocaleDateString("en-US", dateFormat);
