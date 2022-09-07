6d6a4be2a339d80298f525ff43df17b839315bb8
Mark Silva <mark.allen.silva@gmail.com>
2022-09-06 21:14:45 -0700

    clock is automatically updating

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const time = document.getElementById("time");
-const date = document.getElementById("date");
+function newTime() {
+  const time = document.getElementById("time");
+  const date = document.getElementById("date");
 
-const dateFormat = {weekday: 'long', month: 'short', day: 'numeric'};
-const today = new Date();
+  const dateFormat = {weekday: 'long', month: 'short', day: 'numeric'};
+  const today = new Date();
 
 
-const hours = convertHours(today.getHours());
-const minutes = today.getMinutes();
-const seconds = addZero(today.getSeconds());
+  const hours = convertHours(today.getHours());
+  const minutes = addZero(today.getMinutes());
+  const seconds = addZero(today.getSeconds());
 
-function convertHours(num) {
-  return num > 12 ? num - 12 : num;
+  function convertHours(num) {
+    return num > 12 ? num - 12 : num;
+  }
+
+  function addZero(num) {
+    return num < 10 ? `0${num}`: num;
+  }
+
+  const currentTime = `${hours}:${minutes}:${seconds}`;
+
+  time.innerHTML = currentTime;
+  date.innerHTML = today.toLocaleDateString("en-US", dateFormat);
 }
 
-function addZero(num) {
-  return num < 10 ? `0${num}`: num;
-}
-
-const currentTime = `${hours}:${minutes}:${seconds}`;
-
-time.innerHTML = currentTime;
-date.innerHTML = today.toLocaleDateString("en-US", dateFormat);
+setInterval(newTime, 1000);
